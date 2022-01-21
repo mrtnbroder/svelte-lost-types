63eb4dd8eca9a73cb47148bf049b0b555094d456
mrtnbroder <mrtnbroder@gmail.com>
2022-01-21 19:36:04 +0100

    3 files modified, 1 added

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
+import preprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
 };
 
 export default config;
