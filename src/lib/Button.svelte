6b2669f16a5a82abc174dd42e302e9282de902f6
Martin Broder <mrtnbroder@gmail.com>
2022-02-08 16:08:48 +0100

    git init

---
+++ src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  /** CSS class for the element. */
+  let _class: string | null = null
+  export { _class as class }
+
+  /** Whether the button is disabled. */
+  export let disabled: boolean | null = null
+
+
+</script>
+
+<!-- when using $$restProps, the type infos get lost when running `svelte-kit package` -->
+<!-- <button class={_class} {disabled} {...$$restProps}> -->
+<button class={_class} {disabled}>
+  <slot />
+</button>
